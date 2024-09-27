b49772a52f366726585f7db14bd3e5899a5a053f
Laly18t <laly.teissier@gmail.com>
2024-09-27 11:06:29 +0200

    feat: add audioController

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : BaseController<AudioController>
+{
+    [Header("Audio Sources")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("Audio Clip")]
+
+    public AudioClip background;
+    public AudioClip collectable;
+    public AudioClip reveil;
+    public AudioClip reveilBaillement;
+
+
+    private void Start()
+    {
+        musicSource.clip = reveil;
+        musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+}
